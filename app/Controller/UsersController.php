5ecf809ee73fd7ea33cf1a7c5a313bfb85df7106
Himanshu <himanshu.mishra@soarlogic.com>
2015-03-20 22:37:00 +0530

    updated code for testing pls don't pull this time

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -19,26 +19,47 @@ class UsersController extends AppController
 	{
 		$countries = $this->Country->getCountryList();
 		$this->set('countries', $countries);
-
 		if ($this->request->is('post')) {
 			$data = $this->request->data;
 			$this->User->set($data);
 			$this->Company->set($data);
-			$userFields = array('prefix', 'first_name', 'last_name', 'username', 'email','password');
-			$companyFields = array('company_name', 'email', 'job_function', 'industry_id', 'web_url', 'vat_id',
+			$userFields = array('prefix', 'first_name', 'last_name', 'username', 'email','password',"is_agree");
+			$companyFields = array('company_name', 'email', 'industry_id','vat_id',
 				'address', 'city', 'postal_code', 'state', 'country_id');
 			$userFlag = $this->User->validates(array('fieldList' => $userFields));
 			$companyFlag = $this->Company->validates(array('fieldList' => $companyFields));
 			if ($userFlag && $companyFlag) {
-				$profileSaved = $this->Company->save($data);
-				if ($profileSaved) {
+				$companySaved = $this->Company->save($data);
+				if ($companySaved) {
 					$companyId = $this->Company->id;
 					$data['User']['company_id'] = $companyId;
+                    $data['User']['group_id']  = 3;//this is making User to the company's Admin
 					$userSaved = $this->User->save($data);
+                    if($userSaved){
+                        $adminMailId =  $data['User']['email'];
+                        $adminPassword = $data['User']['password'];
+                       $sendMail =  $this->__sendMailToAdmin($adminMailId , $adminPassword);
+                        if($sendMail){
+
+                        }
+                    }
 				}
 			}
 		}
 	}
+
+    public function __sendMailToAdmin($adminMail , $adminPassword){
+
+        App::uses ( 'CakeEmail', 'Network/Email' );
+        $email = new CakeEmail("gmail");
+        $email->emailFormat('html');
+        $email->from('admin@greece-info.com');
+        $email->to($adminMail);
+        $email->subject("Trail User Registration");
+        $message = "<div>Hello,</div><div>UserName:" . $adminMail .
+            "</div><div>Password: " . $adminPassword . "</div>";
+        $email->send($message);
+    }
 
 	public function login()
 	{
---
+++ app/Model/Group.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Himanshu
+ * Date: 20/3/15
+ * Time: 7:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Group extends AppModel
+{
+
+    public  $useTable = 'groups';
+
+    public $name = 'Group';
+}
